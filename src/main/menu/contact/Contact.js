b8c5b14394e41eaecd0cee6cef728858912e92de
Leo espinoza <leo.spinoza@yahoo.com>
2018-12-04 14:04:52 -0600

    added contact component

--- src/main/menu/contact/Contact.js
+++ src/main/menu/contact/Contact.js
@@ -12,8 +12,8 @@ const Contact = (props) => {
       <input type="text" placeholder='Fullname' required/>
       <input type="email" placeholder='Email Address' required/>
       <textarea name="" id="" cols="30" rows="10" placeholder='Enter your message'></textarea>
-     
-   
+      <input type="submit" onClick={props.click}/>
+      <p><b>* </b>All your inquiries are important to us. Kindly provide your contact details and message and we will get back to you as soon as possible.</p>
 
 
     </div>
